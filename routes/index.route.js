f408fa5bf12679b937cb90b899711a58a050cd1d
Dimas Miftahul Huda <dimas.mfth@gmail.com>
2022-07-23 11:34:45 +0700

    feat: add routes

--- routes/index.route.js
+++ routes/index.route.js
@@ -1,10 +1,16 @@
 const indexController = require('../controllers/index.controller');
 const postController = require('../controllers/post.controller');
+const galeriController = require('../controllers/galeri.controller');
+const tempatController = require('../controllers/tempat.controller');
+const kulinerController = require('../controllers/kuliner.controller');
 const auth = require('../middlewares/auth');
 
 const router = require('express').Router();
 
 router.get('/', indexController.index);
+router.get('/galeri', galeriController.index);
+router.get('/tempat', tempatController.index);
+router.get('/kuliner', kulinerController.index);
 
 router.get('/posts', postController.index);
 router.get('/posts/create', postController.create);
